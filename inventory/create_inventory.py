c7475b7a3cf5afa742307e3b7e552333c5fa130a
Pashtet <pavel.pivikov2008@yandex.ru>
2025-01-16 23:47:53 +0300

    Concept

--- inventory/create_inventory.py
+++ inventory/create_inventory.py
@@ -38,13 +38,15 @@ font = pygame.font.Font(load_font('Courier WGL4 Italic.otf'), 35)
 font_small = pygame.font.Font(load_font('Courier WGL4 Italic.otf'), 25)
 
 
-class Board:
+class Inventory:
     def __init__(self, width, height):
+        global tab_inventory, tab_equipment
         self.con = sqlite3.connect('forproject2.bd')
         self.cursor = self.con.cursor()
-        result = self.cursor.execute("""SELECT equipment FROM Data""").fetchone()[0].split(', ')
-        self.equipment = result
-        print(self.equipment)
+        tab_equipment = self.cursor.execute("""SELECT equipment FROM Data""").fetchone()[0].split(', ')
+        tab_inventory = self.cursor.execute("""SELECT inventory FROM Data""").fetchone()[0].split(', ')
+        tab_inventory[len(tab_inventory) - 1] = tab_inventory[len(tab_inventory) - 1][:-1]
+        self.click = False
         self.width = width
         self.height = height
         self.board = [[0] * width for i in range(height)]
@@ -60,10 +62,6 @@ class Board:
     def render(self, screen):
         pygame.draw.rect(screen, (50, 50, 50), (1080, 0, 520, 900))
         text_inventory = font.render('Инвентарь', True, (255, 255, 255))
-        text_weapon = font_small.render('Оружие', True, (255, 255, 255))
-        text_armor = font_small.render('Броня', True, (255, 255, 255))
-        text_shoes = font_small.render('Обувь', True, (255, 255, 255))
-        text_ring = font_small.render('Кольцо', True, (255, 255, 255))
         image_gold = pygame.transform.scale(load_image('gold.png'), (25, 25))
         result = self.cursor.execute("""SELECT money FROM Data""").fetchone()
         text_money = font_small.render(result[0], True, (255, 255, 0))
@@ -79,43 +77,44 @@ class Board:
                                   self.top + y * self.cell_size_y,
                                  self.cell_size_x, self.cell_size_y), 1)
         screen.blit(text_inventory, (1080 + 260 - text_inventory.get_width() // 2, 20))
-        rect_weapon = pygame.draw.rect(screen, self.color[self.board[y][x]], (1080 + 20, 250, 92, 68))
-        rect_armor = pygame.draw.rect(screen, self.color[self.board[y][x]], (1080 + 210, 250, 92, 68))
-        rect_shoes = pygame.draw.rect(screen, self.color[self.board[y][x]], (1080 + 210, 400, 92, 68))
-        rect_ring = pygame.draw.rect(screen, self.color[self.board[y][x]], (1080 + 400, 250, 92, 68))
-        screen.blit(text_shoes, (1080 + 210, 470))
-        screen.blit(text_armor, (1080 + 210, 320))
-        screen.blit(text_weapon, (1080 + 20, 320))
-        screen.blit(text_ring, (1080 + 400, 320))
         screen.blit(image_gold, (1110, 560))
         self.items()
 
     def items(self):
-        result = self.cursor.execute("""SELECT inventory FROM Data""").fetchone()[0].split(', ')
-        result[len(result) - 1] = result[len(result) - 1][:-1]
-        result = ['Aghanim_Scepter_icon', 'Aghanim_Scepter_icon', 'Aghanim_Scepter_icon', 'Crystalys_icon', 'Crystalys_icon',
-                  'Assault_Cuirass_icon', 'Assault_Cuirass_icon', 'Assault_Cuirass_icon', 'Platemail_icon', 'Platemail_icon']
-        for i in range(0, len(result)):
-            image = load_image(result[i] + '.png')
+        for i in range(0, len(tab_inventory)):
+            image = load_image(tab_inventory[i] + '.png')
             screen.blit(image, (1112 + ((i % 5) * 92), 592 + ((i // 5) * 68)))
-        if len(self.equipment) >= 1:
-            image = load_image(self.equipment[0] + '.png')
-            screen.blit(image, (1102, 252))
 
 
-    def get_cell(self, mouse_pos):
-        x = mouse_pos[0] - 1100
-        y = mouse_pos[1] - 590
-        return (x // 92, y // 68)
+    def clicking_cell(self, mouse_pos):
+        x = (mouse_pos[0] - 1110) // 92
+        y = (mouse_pos[1] - 590) // 68
+        return (x, y, (mouse_pos[0] - 1110) % 92, (mouse_pos[1] - 590) % 68)
 
 
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        #self.on_click(cell)
+    def get_click(self, event):
+        global tab_inventory
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            self.cell = self.clicking_cell(event.pos)
+            if len(tab_inventory) >= self.cell[0] + 1 + self.cell[1] * 5 and self.cell[0] >= 0 and self.cell[1] >= 0:
+                self.click = True
+                self.temp = tab_inventory[(self.cell[0] + 1 * self.cell[1] + 1) - 1]
+                del tab_inventory[(self.cell[0] + 1 * self.cell[1] + 1) - 1]
+        if event.type == pygame.MOUSEBUTTONUP and self.click:
+            self.cell = self.clicking_cell(event.pos)
+            self.click = False
+            if (event.pos[0] < 1100 or event.pos[0] > 1560) or (event.pos[1] > 862 or event.pos[1] < 590):
+                tab_inventory.append(self.temp)
+
+    def get_motion(self, x, y):
+        if self.click:
+            image = load_image(self.temp + '.png')
+            screen.blit(image, (x - self.cell[2], y - self.cell[3]))
 
 
-
-board = Board(5, 4)
+board_inv = Inventory(5, 4)
+click = False
+cell = int()
 running = True
 while running:
     for event in pygame.event.get():
@@ -124,9 +123,12 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_TAB:
                 pass
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            board.get_click(event.pos)
+        board_inv.get_click(event)
+        if event.type == pygame.MOUSEMOTION:
+            x_motion = event.pos[0]
+            y_motion = event.pos[1]
     screen.fill((255, 255, 255))
-    board.render(screen)
+    board_inv.render(screen)
+    board_inv.get_motion(x_motion, y_motion)
     pygame.display.flip()
 pygame.quit()
